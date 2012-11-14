95d91d824c06934f4e0192ebeda7c3bac0932607
SimonLarsen <simonhffh@gmail.com>
2012-11-15 00:38:32 +0100

    Added util module for utility functions

---
+++ util.lua
@@ -0,0 +1,12 @@
+--- Collides two AABBs
+-- @param b1 First bounding box
+-- @param b2 Second bounding box
+-- @return True if they overlap
+function collideBoxes(b1,b2)
+	if b1.x > b2.x+b2.w or b1.x+b1.w < b2.x
+	or b1.y > b2.y+b2.h or b1.y+b1.h < b2.y then
+		return false
+	else
+		return true
+	end
+end
