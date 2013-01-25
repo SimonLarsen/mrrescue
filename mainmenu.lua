6e1d61296dfec342bfbf71455ede43a7f89513cf
SimonLarsen <simonhffh@gmail.com>
2013-01-25 20:17:49 +0100

    Added missing mainmenu module and opening song

---
+++ mainmenu.lua
@@ -0,0 +1,31 @@
+mainmenu = {}
+
+lg = love.graphics
+
+function mainmenu.update(dt)
+end
+
+function mainmenu.draw()
+	lg.push()
+	lg.scale(SCALE)
+
+	lg.drawq(img.splash, quad.splash, 0,0)
+	lg.setFont(font.bold)
+	lg.print("START GAME", 150, 104)
+	lg.print("HIGHSCORES", 150, 117)
+	lg.print("OPTIONS", 150, 130)
+	lg.print("HISTORY", 150, 143)
+	lg.print("EXIT", 150, 156)
+
+	lg.pop()
+end
+
+function mainmenu.keypressed(k, uni)
+	if k == "return" or k == " " then
+		ingame.newGame()
+	end
+end
+
+function mainmenu.joystickpressed(joy, k)
+	
+end
