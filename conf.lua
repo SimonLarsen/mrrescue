ec139833eba2781507cf32d9df30772138a76829
Simon Larsen <simonhffh@gmail.com>
2016-05-11 01:08:29 +0200

    Fixed love.conf being doubly defined

--- conf.lua
+++ conf.lua
@@ -2,29 +2,6 @@ function love.conf(t)
     t.identity = "mrrescue"
     t.version = "0.10.0"
     t.console = false
-
-    t.window.title = "Mr. Rescue"
-    t.window.icon = nil
-    t.window.width = 256*3
-    t.window.height = 200*3
-    t.window.borderless = false
-    t.window.resizable = false
-    t.window.fullscreen = false
-    t.window.fullscreentype = "exclusive"
-    t.window.vsync = true
-    t.window.fsaa = 0
-    t.window.display = 1
-    t.window.highdpi = false
-    t.window.srgb = false
-
-    t.modules.physics = false
-    t.modules.mouse = false
-end
-
-function love.conf(t)
-    t.identity = "mrrescue"
-    t.version = "0.10.0"
-    t.console = false
     t.accelerometerjoystick = true
     t.gammacorrect = false
  
